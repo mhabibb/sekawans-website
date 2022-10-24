1f10e859976e8aaa6b6f5980afcf1057d496b757
Aziz Solihin <azizslhn45@gmail.com>
2022-10-24 14:03:54 +0700

    first commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top" aria-label="Offcanvas navbar large">
+    <div class="container">
+        <a class="navbar-brand" href="#">Sekawan'S</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2"
+            aria-controls="offcanvasNavbar2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end text-bg-light" tabindex="-1" id="offcanvasNavbar2"
+            aria-labelledby="offcanvasNavbar2Label">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbar2Label"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tentang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Info TB</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kasus TB</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Artikel</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kegiatan</a>
+                    </li>
+                </ul>
+                <form class="d-flex mt-3 mt-lg-0" role="search">
+                    <input class="form-control rounded-0 me-1" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-primary rounded-0" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Sekawans TB Jember</title>
+  @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+  <x-navbar />
+
+  <main class="bg-dark" style="min-height: 50vh">
+
+  </main>
+</body>
+
+</html>

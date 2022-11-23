1db150f460edc34a0108ec635365cff966a9b294
Aziz Solihin <azizslhn45@gmail.com>
2022-11-23 16:11:38 +0700

    navigasi profil

--- resources/views/components/admin-nav.blade.php
+++ resources/views/components/admin-nav.blade.php
@@ -41,7 +41,7 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="{{ route('admin.users.show', Auth::user()) }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Profil</p>
                             </a>

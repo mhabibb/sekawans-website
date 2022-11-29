7d560228f3975071325ca421720d4566197156f0
Aziz Solihin <azizslhn45@gmail.com>
2022-11-30 01:20:33 +0700

    ajax trash article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,231 +10,238 @@ use Spatie\Activitylog\Models\Activity;
 
 class ArticleController extends Controller
 {
-    /**
-     * Instantiate a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->authorizeResource(Article::class, 'article');
+  /**
+   * Instantiate a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
+    $this->authorizeResource(Article::class, 'article');
+  }
+
+  /**
+   * Get the map of resource methods to ability names.
+   *
+   * @return array
+   */
+  protected function resourceAbilityMap()
+  {
+    return [
+      'index'       => 'viewAny',
+      'show'        => 'view',
+      'create'      => 'create',
+      'store'       => 'create',
+      'edit'        => 'update',
+      'update'      => 'update',
+      'destroy'     => 'delete',
+      'restore'     => 'restore',
+      'forceDelete' => 'forceDelete'
+    ];
+  }
+
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $category = request()->segments()[1];
+    if ($category == "infos") {
+      $articles = Article::latest()->category(1);
+      $title = 'Info TBC';
+      $createRoute = 'admin.infotbc.create';
+      $showRoute = 'admin.infotbc.show';
+      $editRoute = 'admin.infotbc.edit';
+    } else if ($category == "articles") {
+      $articles = Article::latest()->category(2);
+      $title = 'Artikel';
+      $createRoute = 'admin.articles.create';
+      $showRoute = 'admin.articles.show';
+      $editRoute = 'admin.articles.edit';
+    } else if ($category == "actions") {
+      $articles = Article::latest()->category(3);
+      $title = 'Kegiatan';
+      $createRoute = 'admin.kegiatan.create';
+      $showRoute = 'admin.kegiatan.show';
+      $editRoute = 'admin.kegiatan.edit';
+    } else {
+      abort(404);
+    };
+    (!auth()->user()->role ? $articles->user() : '');
+    $articles = $articles->get();
+    return view('admin.article.index', [
+      'title' => $title,
+      'articles' => $articles,
+      'createRoute' => $createRoute,
+      'showRoute' => $showRoute,
+      'editRoute' => $editRoute
+    ]);
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    $category = request()->segments()[1];
+    if ($category == "infos") {
+      $category = 1;
+      $title = 'Info TBC';
+    } else if ($category == "articles") {
+      $category = 2;
+      $title = 'Artikel';
+    } else if ($category == "actions") {
+      $category = 3;
+      $title = 'Kegiatan';
+    } else {
+      abort(404);
     }
 
-    /**
-     * Get the map of resource methods to ability names.
-     *
-     * @return array
-     */
-    protected function resourceAbilityMap()
-    {
-        return [
-            'index'       => 'viewAny',
-            'show'        => 'view',
-            'create'      => 'create',
-            'store'       => 'create',
-            'edit'        => 'update',
-            'update'      => 'update',
-            'destroy'     => 'delete',
-            'restore'     => 'restore',
-            'forceDelete' => 'forceDelete'
-        ];
+    return view('admin.article.create', [
+      'category' => $category,
+      'title' => $title
+    ]);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \App\Http\Requests\StoreArticleRequest  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(StoreArticleRequest $request)
+  {
+    $request = $request->validated();
+    $request['img'] = $request['img']->store('img/articles');
+    $route = match ($request['category_id']) {
+      '1' => 'admin.infotbc.show',
+      '2' => 'admin.articles.show',
+      '3' => 'admin.kegiatan.show'
+    };
+    $article = Article::create($request);
+    (!$article ? Storage::delete($request['img']) : '');
+    return redirect()->route($route, $article);
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\Article  $article
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Article $article)
+  {
+    if (request()->routeIs('admin.infotbc.show')) {
+      $indexRoute = 'admin.infotbc.index';
+      $editRoute = 'admin.infotbc.edit';
+    } else if (request()->routeIs('admin.articles.show')) {
+      $indexRoute = 'admin.articles.index';
+      $editRoute = 'admin.articles.edit';
+    } else if (request()->routeIs('admin.kegiatan.show')) {
+      $indexRoute = 'admin.kegiatan.index';
+      $editRoute = 'admin.kegiatan.edit';
+    } else {
+      abort(404);
+    }
+    return view('admin.article.show', compact('article', 'indexRoute', 'editRoute'));
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Models\Article  $article
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Article $article)
+  {
+    if (request()->routeIs('admin.infotbc.edit')) {
+      $title = 'Info TBC';
+    } else if (request()->routeIs('admin.articles.edit')) {
+      $title = 'Artikel';
+    } else if (request()->routeIs('admin.kegiatan.edit')) {
+      $title = 'Kegiatan';
+    } else {
+      abort(404);
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $category = request()->segments()[1];
-        if ($category == "infos") {
-            $articles = Article::latest()->category(1);
-            $title = 'Info TBC';
-            $createRoute = 'admin.infotbc.create';
-            $showRoute = 'admin.infotbc.show';
-            $editRoute = 'admin.infotbc.edit';
-        } else if ($category == "articles") {
-            $articles = Article::latest()->category(2);
-            $title = 'Artikel';
-            $createRoute = 'admin.articles.create';
-            $showRoute = 'admin.articles.show';
-            $editRoute = 'admin.articles.edit';
-        } else if ($category == "actions") {
-            $articles = Article::latest()->category(3);
-            $title = 'Kegiatan';
-            $createRoute = 'admin.kegiatan.create';
-            $showRoute = 'admin.kegiatan.show';
-            $editRoute = 'admin.kegiatan.edit';
-        } else {
-            abort(404);
-        };
-        (!auth()->user()->role ? $articles->user() : '');
-        $articles = $articles->get();
-        return view('admin.article.index', [
-            'title' => $title,
-            'articles' => $articles,
-            'createRoute' => $createRoute,
-            'showRoute' => $showRoute,
-            'editRoute' => $editRoute
-        ]);
-    }
+    return view('admin.article.edit', compact('article', 'title'));
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $category = request()->segments()[1];
-        if ($category == "infos") {
-            $category = 1;
-            $title = 'Info TBC';
-        } else if ($category == "articles") {
-            $category = 2;
-            $title = 'Artikel';
-        } else if ($category == "actions") {
-            $category = 3;
-            $title = 'Kegiatan';
-        } else {
-            abort(404);
-        }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \App\Http\Requests\UpdateArticleRequest  $request
+   * @param  \App\Models\Article  $article
+   * @return \Illuminate\Http\Response
+   */
+  public function update(UpdateArticleRequest $request, Article $article)
+  {
+    $validated = $request->validated();
+    if (isset($validated['img'])) {
+      $oldImg = $article->img;
+      $validated['img'] = $validated['img']->store('img/articles');
+    };
+    $article->update($validated);
 
-        return view('admin.article.create', [
-            'category' => $category,
-            'title' => $title
-        ]);
-    }
+    isset($validated['img']) ? ($article->wasChanged() ? Storage::delete($oldImg) : Storage::delete($validated['img'])) : '';
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreArticleRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreArticleRequest $request)
-    {
-        $request = $request->validated();
-        $request['img'] = $request['img']->store('img/articles');
-        $route = match ($request['category_id']) {
-            '1' => 'admin.infotbc.show',
-            '2' => 'admin.articles.show',
-            '3' => 'admin.kegiatan.show'
-        };
-        $article = Article::create($request);
-        (!$article ? Storage::delete($request['img']) : '');
-        return redirect()->route($route, $article);
-    }
+    $category = (request()->segments()[1]);
+    $category == "infos" ? $route = 'admin.infotbc.show'
+      : ($category == "articles" ? $route = 'admin.articles.show'
+        : ($category == "actions" ? $route = 'admin.kegiatan.show' : $route = null));
+    return redirect()->route($route, $article);
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Article $article)
-    {
-        if (request()->routeIs('admin.infotbc.show')) {
-            $indexRoute = 'admin.infotbc.index';
-            $editRoute = 'admin.infotbc.edit';
-        } else if (request()->routeIs('admin.articles.show')) {
-            $indexRoute = 'admin.articles.index';
-            $editRoute = 'admin.articles.edit';
-        } else if (request()->routeIs('admin.kegiatan.show')) {
-            $indexRoute = 'admin.kegiatan.index';
-            $editRoute = 'admin.kegiatan.edit';
-        } else {
-            abort(404);
-        }
-        return view('admin.article.show', compact('article', 'indexRoute', 'editRoute'));
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Article  $article
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Article $article)
+  {
+    $category = $article->category_id;
+    $article->delete();
+    return match ($category) {
+      1 => redirect()->route('admin.infotbc.index'),
+      2 => redirect()->route('admin.articles.index'),
+      3 => redirect()->route('admin.kegiatan.index')
+    };
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Article $article)
-    {
-        if (request()->routeIs('admin.infotbc.edit')) {
-            $title = 'Info TBC';
-        } else if (request()->routeIs('admin.articles.edit')) {
-            $title = 'Artikel';
-        } else if (request()->routeIs('admin.kegiatan.edit')) {
-            $title = 'Kegiatan';
-        } else {
-            abort(404);
-        }
+  public function trash()
+  {
+    $articles = Article::latest()->category(1)->get();
 
-        return view('admin.article.edit', compact('article', 'title'));
-    }
+    return json_encode($articles);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateArticleRequest  $request
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateArticleRequest $request, Article $article)
-    {
-        $validated = $request->validated();
-        if (isset($validated['img'])) {
-            $oldImg = $article->img;
-            $validated['img'] = $validated['img']->store('img/articles');
-        };
-        $article->update($validated);
+  public function restore(Article $article)
+  {
+    $article->getDeletedAtColumn() ? $article->restore() : '';
+    return redirect()->back();
+  }
 
-        isset($validated['img']) ? ($article->wasChanged() ? Storage::delete($oldImg) : Storage::delete($validated['img'])) : '';
-
-        $category = (request()->segments()[1]);
-        $category == "infos" ? $route = 'admin.infotbc.show'
-            : ($category == "articles" ? $route = 'admin.articles.show'
-                : ($category == "actions" ? $route = 'admin.kegiatan.show' : $route = null));
-        return redirect()->route($route, $article);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Article  $article
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Article $article)
-    {
-        $category = $article->category_id;
-        $article->delete();
-        return match ($category) {
-            1 => redirect()->route('admin.infotbc.index'),
-            2 => redirect()->route('admin.articles.index'),
-            3 => redirect()->route('admin.kegiatan.index')
-        };
-    }
-
-    public function restore(Article $article)
-    {
-        $article->getDeletedAtColumn() ? $article->restore() : '';
-        return redirect()->back();
-    }
-
-    public function forceDelete(Article $article)
-    {
-        $ext = collect(explode('.', $article->img))->last();
-        // dd($ext);
-        $base64img = base64_encode(file_get_contents(storage_path('app/public/' . $article->img)));
-        $base64img = "data:image/{$ext};base64, {$base64img}";
-        $ext = explode('/', explode(':', substr($base64img, 0, strpos($base64img, ';')))[1])[1];
-        dd($base64img, $ext);
-        Storage::delete($article->img);
-        $article->forceDelete();
-        $category = $article->category_id;
-        return match ($category) {
-            1 => redirect()->route('admin.infotbc.index'),
-            2 => redirect()->route('admin.articles.index'),
-            3 => redirect()->route('admin.kegiatan.index')
-        };
-    }
+  public function forceDelete(Article $article)
+  {
+    $ext = collect(explode('.', $article->img))->last();
+    // dd($ext);
+    $base64img = base64_encode(file_get_contents(storage_path('app/public/' . $article->img)));
+    $base64img = "data:image/{$ext};base64, {$base64img}";
+    $ext = explode('/', explode(':', substr($base64img, 0, strpos($base64img, ';')))[1])[1];
+    dd($base64img, $ext);
+    Storage::delete($article->img);
+    $article->forceDelete();
+    $category = $article->category_id;
+    return match ($category) {
+      1 => redirect()->route('admin.infotbc.index'),
+      2 => redirect()->route('admin.articles.index'),
+      3 => redirect()->route('admin.kegiatan.index')
+    };
+  }
 }
--- resources/views/admin/article/index.blade.php
+++ resources/views/admin/article/index.blade.php
@@ -16,41 +16,46 @@
       <div class="col-12">
         <div class="card">
           <div class="card-header">
-            <a href="{{ route($createRoute) }}" class="btn btn-primary card-title">Buat {{ $title }} Baru</a>
+            <a href="{{ route($createRoute) }}" class="btn btn-primary">Buat {{ $title }} Baru</a>
+            <button class="mx-1 btn btn-info" id="trashBtn">Lihat Sampah</button>
+            <button class="mx-1 btn btn-info d-none" id="dataBtn">Tutup</button>
           </div>
 
           <div class="card-body">
-            <table id="articlesData" class="table table-bordered table-striped">
-              <thead>
-                <tr>
-                  <th>Judul {{ $title }}</th>
-                  <th>Waktu Update</th>
-                  <th>Author</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($articles as $article)
-                <tr>
-                  <td>{{$article->title}}</td>
-                  <td>{{ date('d M Y, H:i', strtotime($article->updated_at)) }}</td>
-                  <td>{{ $article->user->name }}</td>
-                  <td>
-                    <a href="{{ route($showRoute, $article) }}" class="badge badge-success mr-2">Lihat</a>
-                    <a href="{{route($editRoute , $article)}}" class="badge badge-warning mr-2">
-                      <i class="fa-solid fa-pen-to-square"></i> Edit</a>
-                    <form action="{{route('admin.articles.destroy', $article)}}" method="POST" class="d-inline-block">
-                      @method('DELETE')
-                      @csrf
-                      <button class="badge badge-danger border-0"
-                        onclick="return confirm('Yakin untuk menghapus {{ $article->title }} ?')">
-                        <i class="fa-solid fa-trash-can"></i> Hapus</button>
-                    </form>
-                  </td>
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
+            <div id="data-table">
+              <table id="articlesData" class="table table-bordered table-striped mx-auto">
+                <thead>
+                  <tr>
+                    <th>Judul {{ $title }}</th>
+                    <th>Waktu Update</th>
+                    <th>Author</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($articles as $article)
+                  <tr>
+                    <td>{{$article->title}}</td>
+                    <td>{{ date('d M Y, H:i', strtotime($article->updated_at)) }}</td>
+                    <td>{{ $article->user->name }}</td>
+                    <td>
+                      <a href="{{ route($showRoute, $article) }}" class="badge badge-success mr-2">Lihat</a>
+                      <a href="{{route($editRoute , $article)}}" class="badge badge-warning mr-2">
+                        <i class="fa-solid fa-pen-to-square"></i> Edit</a>
+                      <form action="{{route('admin.articles.destroy', $article)}}" method="POST" class="d-inline-block">
+                        @method('DELETE')
+                        @csrf
+                        <button class="badge badge-danger border-0"
+                          onclick="return confirm('Yakin untuk menghapus {{ $article->title }} ?')">
+                          <i class="fa-solid fa-trash-can"></i> Hapus</button>
+                      </form>
+                    </td>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+            <table id="trashData" class="table table-striped mx-auto d-none"></table>
           </div>
         </div>
       </div>
@@ -70,6 +75,47 @@
       order: [[1, 'desc']],
     });
   });
+</script>
+
+<script>
+  $("#trashBtn").click(function() {
+    $('#data-table').addClass('d-none');
+    $('#dataBtn').removeClass('d-none');
+    $('#trashBtn').addClass('d-none');
+    const ajax = new XMLHttpRequest();
+    ajax.onload = function() {
+      if (ajax.status == 200) {
+        const responses = JSON.parse(ajax.responseText);
+        $('#trashData').removeClass('d-none');
+        showData(responses);
+      } else {
+        alert("Maaf, telah terjadi kesalahan.");
+      }
+    };
+    ajax.open('GET', '{{ route('admin.articles.trash') }}');
+    ajax.send();
+  })
+
+  $('#dataBtn').click(function() {
+      $('#trashBtn').removeClass('d-none');
+      $('#trashData').addClass('d-none');
+      $('#data-table').removeClass('d-none');
+      $(this).addClass('d-none');
+    })
+
+    function showData(responses) {
+      let text = `<thead><tr><th colspan="3"> Data Terhapus </th></tr></thead><tbody>`
+      for (let x in responses) {
+        text += `<tr>
+          <td>` + responses[x].title + `</td>
+          <td>` + responses[x].user.name + `</td>
+          <td class="d-flex"><button class="badge badge-success border-0">
+            <i class="fa-solid fa-rotate-left"></i>Restore</button>
+          </td></tr>`;
+      }
+      text += "</tbody>";
+      $('#trashData').html(text);
+    }
 </script>
 
 @endsection

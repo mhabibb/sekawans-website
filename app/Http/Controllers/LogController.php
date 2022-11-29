4b03fd156caadc6117e760d3a20b9e57b1c3f807
Aziz Solihin <azizslhn45@gmail.com>
2022-11-30 01:20:55 +0700

    halaman log

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -7,47 +7,47 @@ use Spatie\Activitylog\Models\Activity;
 
 class LogController extends Controller
 {
-    /**
-     * Instantiate a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+  /**
+   * Instantiate a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $this->authorize('superAdmin');
-        $log = Activity::all();
-        return dd($log);
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $this->authorize('superAdmin');
+    $logs = Activity::where('event', 'updated')->get();
+    return view('admin.activitylog.index', compact('logs'));
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Activity  $activity
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Activity $activity)
-    {
-        return $activity;
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\Activity  $activity
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Activity $activity)
+  {
+    return $activity;
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Activity  $activity
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Activity $activity)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Activity  $activity
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Activity $activity)
+  {
+    //
+  }
 }
---
+++ resources/views/admin/activitylog/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+@section('admin-content')
+<section class="content-header">
+  <div class="container-fluid">
+    <h1>Log Aktivitas</h1>
+  </div>
+</section>
+
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table table-striped">
+                @forelse ($logs as $log)
+                <tr>
+                  <td>{{ $log->description }}</td>
+                  <td>
+                    <button class="btn badge badge-success border-0">Restore</button>
+                  </td>
+                </tr>
+                @empty
+                <tr>
+                  <td>Log Kosong</td>
+                </tr>
+                @endforelse
+              </table>
+            </div>
+          </div>
+          <div class="card-footer"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+@endsection

c523d81d67326f7207c6ef2386fb0b47eba12dae
drhadityo <dohanrizqi9@gmail.com>
2024-03-21 12:20:04 +0700

    nama tabel sudah berubah, tipe data juga, done bwosssss

--- database/migrations/2024_03_06_213737_create_screenings_table.php
+++ database/migrations/2024_03_06_213737_create_screenings_table.php
@@ -17,15 +17,18 @@ class CreateScreeningsTable extends Migration
             $table->integer('age');
             $table->string('district');
             $table->date('screening_date');
-            $table->enum('contact_with_tb', ['Ya', 'Tidak']);
-            $table->enum('batuk', ['Ya', 'Tidak']);
-            $table->enum('sesak_nafas', ['Ya', 'Tidak']);
-            $table->enum('keringat_malam_hari', ['Ya', 'Tidak']);
-            $table->enum('demam_meriang', ['Ya', 'Tidak']);
-            $table->enum('ibu_hamil', ['Ya', 'Tidak']);
-            $table->enum('lansia', ['Ya', 'Tidak']);
-            $table->enum('diabetes_melitus', ['Ya', 'Tidak']);
-            $table->enum('merokok', ['Ya', 'Tidak']);
+            $table->boolean('home_contact', ['1', '0']);
+            $table->boolean('close_contact', ['1', '0']);
+            $table->boolean('cough', ['1', '0']);
+            $table->boolean('breath', ['1', '0']);
+            $table->boolean('sweat', ['1', '0']);
+            $table->boolean('fever', ['1', '0']);
+            $table->boolean('weight_loss', ['1', '0']);
+            $table->boolean('pregnant', ['1', '0']);
+            $table->boolean('elderly', ['1', '0']);
+            $table->boolean('diabetes', ['1', '0']);
+            $table->boolean('smoking', ['1', '0']);
+            $table->boolean('ever_treatment', ['1', '0']);
             $table->string('contact1_name');
             $table->string('contact1_number');
             $table->string('contact2_name');
@@ -42,3 +45,5 @@ class CreateScreeningsTable extends Migration
         Schema::dropIfExists('patient_statuses');
     }
 }
+
+?>
